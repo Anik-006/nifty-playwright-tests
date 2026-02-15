208e9f86548685dd3df1183582126b8e62add330
Anik <anik.cse006@gmail.com>
2026-02-15 22:35:46 +0600

    updated

--- playwright.config.js
+++ playwright.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: false,
+    headless: true,
     baseURL: 'http://localhost:5004',
     trace: 'on-first-retry',
   },
