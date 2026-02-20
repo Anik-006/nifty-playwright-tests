cec5252f93e9d945b078ab89eb3b70509803d65e
Anik <anik.cse006@gmail.com>
2026-02-20 12:15:15 +0600

    Checked

--- playwright.config.js
+++ playwright.config.js
@@ -1,30 +1,14 @@
-// playwright.config.js
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
 
   use: {
-    baseURL: 'http://localhost:5004', // points to your frontend server
+    baseURL: 'http://localhost:5004',
     headless: true,
     trace: 'on-first-retry',
     viewport: { width: 1280, height: 720 },
-    ignoreHTTPSErrors: true,
   },
-
-  // Only reuse the server started manually in CI
-  webServer: process.env.CI
-  ? {
-      command: 'npm run dev',
-      url: 'http://localhost:5004',
-      timeout: 120_000,
-      reuseExistingServer: false,
-    }
-  : {
-      command: 'npm run dev',
-      url: 'http://localhost:5004',
-      reuseExistingServer: true,
-    },
 
   projects: [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
@@ -32,15 +16,8 @@ export default defineConfig({
     { name: 'webkit', use: { ...devices['Desktop Safari'] } },
   ],
 
-  /* Run tests in files in parallel */
   fullyParallel: true,
-
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-
-  /* Fail the build on CI if test.only is left in source */
   forbidOnly: !!process.env.CI,
-
-  /* Reporter */
   reporter: [['list'], ['html']],
 });
